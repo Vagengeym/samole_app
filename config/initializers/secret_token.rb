a411890f5be675529a67eaff31197d8902890a64
Vagen <vagengeym@gmail.com>
2013-12-16 02:12:52 +0400

    Yeah

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamoleApp::Application.config.secret_token = '2dadd2993b4ee8bf97a0e4f4e226c679b59da4a0ff4c97daeeb10b411e2c97455a7b9ed3db7ff11314921612577e0750345801849bbf80604711fdaa6c8bd3d1'
